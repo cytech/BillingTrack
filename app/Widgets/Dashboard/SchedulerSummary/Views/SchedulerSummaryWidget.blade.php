9ae6e9bc771a40d3b878cbac96de2249512842f8
dave_albright <cytech@cytech-eng.com>
2018-07-27 14:14:37 -0700

    FusionInvoiceFOSS alpha 4.0.0 - integrate scheduler

--- app/Widgets/Dashboard/SchedulerSummary/Views/SchedulerSummaryWidget.blade.php
+++ app/Widgets/Dashboard/SchedulerSummary/Views/SchedulerSummaryWidget.blade.php
@@ -2,7 +2,7 @@
     <section class="content">
         <div class="box box-solid">
             <div class="box-header">
-                <h3 class="box-title">{{ trans('Scheduler::texts.schedule_summary') }}</h3>
+                <h3 class="box-title">{{ trans('fi.schedule_summary') }}</h3>
             </div>
             <div class="box-body">
                 <div class="row">
@@ -10,11 +10,11 @@
                         <div class="small-box bg-olive">
                             <div class="inner">
                                 <div class="huge"><h3>{!! $schedulerEvents['monthEvent'] !!}</h3></div>
-                                <p>{{ trans('Scheduler::texts.events_this_month') }}</p>
+                                <p>{{ trans('fi.events_this_month') }}</p>
                             </div>
                             <div class="icon"><i class="ion ion-calendar"></i></div>
                             <a class="small-box-footer" href="{!! route('scheduler.fullcalendar') !!}">
-                                {{ trans('Scheduler::texts.vevents_this_month') }}
+                                {{ trans('fi.vevents_this_month') }}
                                 <i class="fa fa-arrow-circle-right"></i>
                             </a>
                         </div>
@@ -23,11 +23,11 @@
                         <div class="small-box bg-orange">
                             <div class="inner">
                                 <div class="huge"><h3>{!! $schedulerEvents['lastMonthEvent'] !!}</h3></div>
-                                <p>{{ trans('Scheduler::texts.events_last_month') }}</p>
+                                <p>{{ trans('fi.events_last_month') }}</p>
                             </div>
                             <div class="icon"><i class="ion ion-calendar"></i></div>
                             <a class="small-box-footer" href="{!! route('scheduler.fullcalendar') !!}?status=last">
-                                {{ trans('Scheduler::texts.vevents_last_month') }}
+                                {{ trans('fi.vevents_last_month') }}
                                 <i class="fa fa-arrow-circle-right"></i>
                             </a>
                         </div>
@@ -36,11 +36,11 @@
                         <div class="small-box bg-yellow">
                             <div class="inner">
                                 <div class="huge"><h3>{!! $schedulerEvents['nextMonthEvent'] !!}</h3></div>
-                                <p>{{ trans('Scheduler::texts.events_next_month') }}</p>
+                                <p>{{ trans('fi.events_next_month') }}</p>
                             </div>
                             <div class="icon"><i class="ion ion-calendar"></i></div>
                             <a class="small-box-footer" href="{!! route('scheduler.fullcalendar') !!}?status=next">
-                                {{ trans('Scheduler::texts.vevents_next_month') }}
+                                {{ trans('fi.vevents_next_month') }}
                                 <i class="fa fa-arrow-circle-right"></i>
                             </a>
                         </div>
@@ -49,11 +49,11 @@
                         <div class="small-box bg-blue">
                             <div class="inner">
                                 <div class="huge"><h3>{!! count($schedulerEvents['reminders']) !!}</h3></div>
-                                <p>{{ trans('Scheduler::texts.reminders') }}</p>
+                                <p>{{ trans('fi.reminders') }}</p>
                             </div>
                             <div class="icon"><i class="ion ion-ios-bell-outline"></i></div>
                             <a class="small-box-footer" href="{!! route('scheduler.index') !!}">
-                                {{ trans('Scheduler::texts.schedule_dashboard') }}
+                                {{ trans('fi.schedule_dashboard') }}
                                 <i class="fa fa-arrow-circle-right"></i>
                             </a>
                         </div>
