f30b4d716878888c6697b30668aef3e343cf0265
dave_albright <cytech@cytech-eng.com>
2018-10-17 07:43:41 -0700

    release 4.0.2

---
+++ database/migrations/2018_10_17_000100_version_402.php
@@ -0,0 +1,33 @@
+<?php
+
+use FI\Modules\Settings\Models\Setting;
+use Illuminate\Database\Migrations\Migration;
+
+class Version402 extends Migration
+{
+
+    /**
+     * Run the migrations.
+     * @table payments_custom
+     *
+     * @return void
+     */
+    public function up()
+    {
+        deleteTempFiles();
+        deleteViewCache();
+
+        Setting::saveByKey('version', '4.0.2');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       //
+     }
+}
